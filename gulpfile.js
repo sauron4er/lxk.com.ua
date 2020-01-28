122ccf6c28fbcf53da227adb4577609005103f21
Stanislav Khramtsov <sauron4er@gmail.com>
2020-01-28 09:19:59 +0200

    styles changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,16 @@ let gulp = require('gulp'),
     pug = require('gulp-pug');
 
 gulp.task('pug', function() {
-    // index дублюємо в корні і в папці en для зручності посилань
-    // let index = gulp.src(['pug/pages/index.pug'])
-    //     .pipe(pug({
-    //         pretty:true
-    //     }))
-    //     .pipe(gulp.dest('.'))
-    //     .pipe(gulp.dest('./en'));
-
-    // інші файли не дублюємо
     // return gulp.src(['pug/pages/index.pug', 'pug/pages/*/*.pug', 'pug/pages/*/*/*.pug'])
     return gulp.src(['pug/pages/*/*.pug', 'pug/pages/*/*/*.pug'])
+        .pipe(pug({
+            pretty:true
+        }))
+        .pipe(gulp.dest('.'))
+});
+
+gulp.task('index', function() {
+    return gulp.src(['pug/pages/*/index.pug'])
         .pipe(pug({
             pretty:true
         }))
