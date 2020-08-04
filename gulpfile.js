9e28effcccc1ead418f5cf9ef7f7228afef16e34
Stanislav Khramtsov <sauron4er@gmail.com>
2020-08-04 10:01:49 +0300

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ let gulp = require('gulp'),
     pug = require('gulp-pug');
 
 gulp.task('pug', function() {
-    return gulp.src(['pug/pages/index.pug', 'pug/pages/*/*.pug', 'pug/pages/*/*/*.pug'])
-    // return gulp.src(['pug/pages/*/*.pug', 'pug/pages/*/*/*.pug'])
+    // return gulp.src(['pug/pages/index.pug', 'pug/pages/*/*.pug', 'pug/pages/*/*/*.pug'])
+    return gulp.src(['pug/pages/*/*.pug', 'pug/pages/*/*/*.pug'])
         .pipe(pug({
             pretty:true
         }))
